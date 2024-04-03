8df9a7e3de80f818a6990c05b8de3fcbb08b6245
Joshua Eliassaint <josheliass0502@gmail.com>
2024-04-03 17:22:53 -0400

    finished settings

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
+    public GameObject settings;
+    public Button startButton;
+    public Button settingsButton;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +19,18 @@ public class GameController : MonoBehaviour
     void Update()
     {
         
+    }
+    public void openSettings()
+    {
+        settings.SetActive(true);
+        startButton.enabled = false;
+        settingsButton.enabled = false;
+    }
+
+    public void closeSettings()
+    {
+        settings.SetActive(false);
+        startButton.enabled = true;
+        settingsButton.enabled = true;
     }
 }
