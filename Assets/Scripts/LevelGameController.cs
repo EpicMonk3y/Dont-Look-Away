1c924fa66019d91e407e01357e49de49bb622ca1
Joshua Eliassaint <josheliass0502@gmail.com>
2024-04-03 23:03:59 -0400

    added more things

---
+++ Assets/Scripts/LevelGameController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGameController : MonoBehaviour
+{
+    public bool settingsActive = false;
+    public GameObject settings;
+    public FirstPersonMovement firstPersonMovement;
+    public SeeEnemy seeEnemy;
+    public FirstPersonLook firstPersonLook;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Settings")){
+
+            if (!settingsActive)
+            {
+                openSettings();
+            }
+        }
+            
+        
+    }
+
+    public void openSettings()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        settings.SetActive(true);
+        firstPersonLook.enabled = false;
+        seeEnemy.enabled = false;
+        firstPersonMovement.enabled = false;
+        settingsActive = true;
+        Time.timeScale = 0f;
+    }
+
+    public void closeSettings()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        settings.SetActive(false);
+        firstPersonLook.enabled = true;
+        seeEnemy.enabled = true;
+        firstPersonMovement.enabled = true;
+        settingsActive = false;
+        Time.timeScale = 1f;
+    }
+}
